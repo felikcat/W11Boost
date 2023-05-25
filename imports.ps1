1372a629d577e27f42103f2b4d24b3e14bc49739
felikcat <29991266+felikcat@users.noreply.github.com>
2023-05-25 00:57:06 -0700

    No commit message

--- imports.ps1
+++ imports.ps1
@@ -6,11 +6,12 @@ $_WIN32_BUILDNUMBER = (Get-WmiObject Win32_OperatingSystem).BuildNumber
 $_WINDOWS_EDITION = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name ProductName
 $_BITS_ARGS = "-MaxDownloadTime 120 -RetryInterval 60 -RetryTimeout 300 -TransferPolicy Unrestricted"
 
-function PolEdit_HCKU
+function PolEdit_HKCU
 {
-    Set-PolicyFileEntry -Path "$env:windir\System32\GroupPolicy\User\registry.pol" -Key
+    Set-PolicyFileEntry -Path "$env:windir\System32\GroupPolicy\User\registry.pol" -Key @args
 }
+
 function PolEdit_HKLM
 {
-    Set-PolicyFileEntry -Path "$env:windir\System32\GroupPolicy\Machine\registry.pol" -Key
+    Set-PolicyFileEntry -Path "$env:windir\System32\GroupPolicy\Machine\registry.pol" -Key @args
 }
