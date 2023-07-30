6e7cad477765e9a51f980268864350fd2fcd1521
felikcat <29991266+felikcat@users.noreply.github.com>
2023-07-30 09:35:19 -0700

    v2.2-rc1

--- Extras/Self-hosted_AI.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd %~dp0
-
-.\..\Third-party\MinSudo.exe --NoLogo powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process powershell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "".\Self-hosted_AI.ps1""' -Verb RunAs}"
--- Extras/Self-hosted_AI.ps1
+++
@@ -1,8 +0,0 @@
-#region Increase pagefile to total RAM * 3 + 257MB.
-$DESIRED_PAGEFILE = (Get-CimInstance Win32_PhysicalMemoryArray).MaxCapacity * 1KB/1MB * 3 + 257
-$PageFile = Get-CimInstance -ClassName Win32_PageFileSetting -Filter "Name like '%pagefile.sys'"
-$PageFile | Remove-CimInstance
-$PageFile = New-CimInstance -ClassName Win32_PageFileSetting -Property @{ Name= "C:\pagefile.sys" }
-$PageFile | Set-CimInstance -Property @{ InitialSize = $DESIRED_PAGEFILE; MaximumSize = $DESIRED_PAGEFILE }
-#endregion
-
