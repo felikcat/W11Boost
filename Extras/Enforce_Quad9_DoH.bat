6a90a40953da2e5baf90d8fcf52d11c8f142664f
felikcat <29991266+felikcat@users.noreply.github.com>
2023-10-05 08:39:48 -0700

    More GUI progress

--- Extras/Enforce_Quad9_DoH.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd %~dp0
-
-.\..\Third-party\NanaRun\MinSudo.exe --NoLogo powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process powershell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "".\Enforce_Quad9_DoH.ps1""' -Verb RunAs}"
